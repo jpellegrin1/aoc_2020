2ef92bc2ebf4a3aca341b68b02c0152723080321
James Pellegrin <jamesp@98point6.com>
2020-12-03 09:37:12 -0800

    Updated for part 2

--- day03/src/main.hs
+++ day03/src/main.hs
@@ -1,18 +1,29 @@
-tobogganRide :: [String] -> Int
+import System.Environment
+
+tobogganRide :: [String] -> (Int, Int) -> Int
 checkTree :: Char -> Bool
-countTree :: Int -> Int -> [String] -> Int
+countTree :: Int -> Int -> (Int, Int) -> Int -> [String] -> Int
+multList :: [Int] -> Int
+
+multList [] = 1
+multList (x:xs) = x * multList xs
 
 checkTree a = a == '#'
 
-countTree n m [] = n
-countTree n m (x:xs) =
-    if checkTree $ x !! (m `mod` 31)
-        then countTree (n+1) (m+3) xs
-        else countTree n (m+3) xs
+countTree n m (a,b) r [] = n
+countTree n m (a,b) r (x:xs) =
+    if (checkTree $ (x !! (m `mod` (length x)))) && ((r+1) `mod` a) == 0
+        then countTree (n+1) (m+b) (a,b) (r+1) xs
+        else countTree n (m+b) (a,b) (r+1) xs
 
-tobogganRide c = countTree 0 0 c
+tobogganRide c (a,b) = countTree 0 0 (a,b) 0 c
 
 main = do
     f <- readFile "input.txt"
     let c = lines f
-    print $ tobogganRide $ c
+        l = ([tobogganRide c (1,1)
+            ,tobogganRide c (1,3)
+            ,tobogganRide c (1,5)
+            ,tobogganRide c (1,7)
+            ,tobogganRide c (2,1)])
+    print $ multList l
